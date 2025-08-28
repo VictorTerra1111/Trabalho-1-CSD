2c64eec60591de8050b621e2b59355cea497ecc8
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-08-28 13:33:05 +0000

    made base clock divisor

--- src/clock_divisor.v
+++ src/clock_divisor.v
@@ -1,39 +1,25 @@
-module clock_divisor (
-    input logic clock1M,
-    input logic reset,
-    output logic clk_10KHz,
-    output logic clk_100KHz
+module clock_divisor(
+    input logic clk_100MHz_i,
+    input logic reset_i,
+    output reg  second_pulse_o
 );
 
-  reg [7:0] clk10_cont;
-  reg [7:0] clk100_cont;
+ // as we need to count to a hundred million for a second, the counter sould go from 27:0,
+ // so it can store 134 217 728
+  
+  reg [27:0] counter; 
 
-  // clk_100KHz
-  always @(posedge clock1M or posedge reset) begin
-    if (reset) begin
-      clk_100KHz <= 0;
-      clk100_cont <= 0;
+  always @(posedge clk_100MHz_i or posedge reset_i) begin
+    if (reset_i) begin
+      counter <= 0;
+      second_pulse_o <= 0;
     end else begin
-      if (clk100_cont == 4) begin
-        clk_100KHz <= ~clk_100KHz;
-        clk100_cont <= 0;
+      if (counter == 100_000_000 - 1) begin
+        counter <= 0;
+        second_pulse_o <= 1;
       end else begin
-        clk100_cont <= clk100_cont + 1;
-      end
-    end
-  end
-
-  // clk_10KHz
-  always @(posedge clock1M or posedge reset) begin
-    if (reset) begin
-      clk_10KHz <= 0;
-      clk10_cont <= 0;
-    end else begin
-      if (clk10_cont == 49) begin
-        clk_10KHz <= ~clk_10KHz;
-        clk10_cont <= 0;
-      end else begin
-        clk10_cont <= clk10_cont + 1;
+        counter <= counter + 1;
+        second_pulse_o <= 0;
       end
     end
   end
